69ca908ea05818ea4107a30e88f3c58386c079b4
Laercio Carvalho Silva <laercio_ti-profissional@yahoo.com.br>
2022-11-29 11:07:30 -0300

    Correcao na classe AlunoMensal.java que mostrava null na hora de executar no metodo main.

--- src/padrao/AlunoSemestral.java
+++ src/padrao/AlunoSemestral.java
@@ -1,9 +1,10 @@
 package padrao;
 
 public class AlunoSemestral extends Aluno{
+    String NomeAlunoSemestral;
     AlunoSemestral(){
         Aluno aluno = new Aluno();
-        //aluno.setNome();
+        aluno.setNome(NomeAlunoSemestral);
         System.out.println("Seja nem vindo "+aluno.getNome() );
         aluno.getNome();
         System.out.println("Ficamos felizes por ser um Aluno Semestral ");
